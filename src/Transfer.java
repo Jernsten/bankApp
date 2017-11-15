7cb0490308f0a34c9ec253930ce4229ff0af646e
Kemal Demirtas <demirtas@kth.se>
2017-11-15 15:58:04 +0100

    vafan

--- src/Transfer.java
+++ src/Transfer.java
@@ -2,8 +2,13 @@ public class Transfer {
 
     public void takeIn(Wallet wallet, int amount) {
         wallet.setSaldo(wallet.getSaldo() + amount);
+        
     }
-    public void swish(){
-
+    
+    public void swish(Wallet wallet, String phoneNumber){
+        
+        //swishing away money
+        wallet.get
+    
     }
 }

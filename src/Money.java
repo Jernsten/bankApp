14984c3cb359b4bee9909e41d5d856ecd1088b07
anno0406 <angelica.nordqvist@gmail.com>
2017-11-15 15:15:55 +0100

    removed enums and added Class Money with constants instead.

--- src/Money.java
+++ src/Money.java
@@ -1,7 +1,8 @@
-public enum Money {
-    FEMHUNDRALAPP,
-    HUNDRALAPP,
-    TJUGOLAPP,
-    TIA,
-    FEMMA;
+public class Money {
+
+    public static final int FEMTIO_LAPP = 50;
+    public static final int HUNDRA_LAPP = 100;
+    public static final int FEMHUNDRA_LAPP = 500;
+    public static final int TUSEN_LAPP = 1000;
+
 }

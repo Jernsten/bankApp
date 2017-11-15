7eca11d88310d338b85318f81b21a8dfcad88713
Kemal Demirtas <demirtas@kth.se>
2017-11-15 15:01:54 +0100

    Set fields for Wallet.class

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,2 +1,25 @@
 public class Wallet {
+    private double saldo;
+    private String owner;
+    
+    public Wallet(double saldo, String owner) {
+        this.saldo = saldo;
+        this.owner = owner;
+    }
+    
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    public String getOwner() {
+        return owner;
+    }
+    
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
